d83b782da4614048600f07403522670ede734073
diulll <badiulfikri24@gmail.com>
2026-01-20 20:20:17 +0700

    feat: Add ongkir relations to Kota model

--- app/Models/Kota.php
+++ app/Models/Kota.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Kota extends Model
 {
@@ -12,5 +13,21 @@ class Kota extends Model
     public function propinsi(): BelongsTo
     {
         return $this->belongsTo(Propinsi::class);
+    }
+
+    /**
+     * Ongkir dari kota ini sebagai kota asal
+     */
+    public function ongkirDari(): HasMany
+    {
+        return $this->hasMany(Ongkir::class, 'kota_asal_id');
+    }
+
+    /**
+     * Ongkir ke kota ini sebagai kota tujuan
+     */
+    public function ongkirKe(): HasMany
+    {
+        return $this->hasMany(Ongkir::class, 'kota_tujuan_id');
     }
 }
